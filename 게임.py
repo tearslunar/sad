09d5175cc45bc7f2f5e72df30e0a99c7f3b4714b
tears <tearscg@gmail.com>
2023-09-03 22:14:43 +0900

    9/3 first

--- 게임.py
+++ 게임.py
@@ -25,24 +25,30 @@ class Button:
         if x + width > mouse[0] > x and y + height > mouse[1] > y:
             screen.blit(img_act,(x_act, y_act))
             if click[0] and action != None:
-                time.sleep(1)
-                action()
+                screen_set(start_background, 0, 0)
+#                time.sleep(1)
         else:
             screen.blit(img_in,(x,y))
+            
+#class click:
+#    def __init__(self, x, y):
+#        mouse = pygame.mouse.get_pos()
+#        click + pygame.mouse.get_pressed()
             
 pygame.init()
 width , height = 420, 690
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Tears_identity.Beta")
 
-bitmap_2 = pygame.cursors.Cursor(
-    (24, 24), (0, 0), *pygame.cursors.compile(pygame.cursors.thickarrow_strings)
-)
-pygame.mouse.set_cursor(bitmap_2)
+#마우스포인터 커스텀으로 변경
+diamond = pygame.cursors.diamond
+pygame.mouse.set_cursor(diamond)
 
 
-#이미지 미리 불러오기
+#이미지 미리 불러오기   
 start_background = pygame.image.load("사진\시작화면.png")
+start_on = pygame.image.load("사진\START1.png")
+start_off = pygame.image.load("사진\START.png")
 
 #시작화면 이미지 설정
 screen_set(start_background, 0, 0)
@@ -52,6 +58,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running  = False 
+    Button(start_off,235,235,158,61,start_on,235,235,1)
     pygame.display.update()
 
 status.close()
--- 좌표.py
+++ 좌표.py
@@ -2,11 +2,11 @@ import pygame
 
 a = 0
 pygame.init()
-width , height = 1280 , 700
+width , height = 420, 690
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Tears_Roguelike.Beta")
 
-start_background = pygame.image.load("시작화면1.png")
+start_background = pygame.image.load("사진\시작화면.png")
 def screen_set(a, b, c):
     screen.blit(a, (b, c))
 screen_set(start_background, 0, 0)
@@ -16,7 +16,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running  = False 
-    for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             print(pygame.mouse.get_pos())
     pygame.display.update()
